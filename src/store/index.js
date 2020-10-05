f25dcb2388d864e7eeeac7655f64a42c70ed8491
Kaelan Richards <kadokaelan@gmail.com>
2020-10-05 12:59:22 -0400

    added lease info store module

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,11 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import leaseInfo from "./modules/lease-info";
 
 Vue.use(Vuex);
+
+export default new Vuex.Store({
+  modules: {
+    leaseInfo,
+  },
+});
